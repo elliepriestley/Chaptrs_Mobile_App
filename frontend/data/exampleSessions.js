7fa531dcc2ab5927223700dac96b8bb61ff23cf2
1sAndZeros <rikie_p@hotmail.com>
2023-11-19 17:22:27 +0000

    update homepage

---
+++ frontend/data/exampleSessions.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    id: 1,
+    date: '20/12/23',
+    time: '17:00 GMT',
+    location: 'London, Bermondsey Corner',
+    chosen_book: {
+      authors: ['Herman Merville'],
+      title: 'Moby Dick',
+      cover_photo:
+        'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ8iGnc5gbcxN18ZZMDBbYaIIuD3Yl8c7k-KH8FFaLdpz6XuAdV',
+    },
+    bookclub: {
+      name: 'The Boundless Bookworms',
+      image:
+        'https://media.glamour.com/photos/5ebd7bfdff61267e6b09e86c/1:1/w_1217,h_1217,c_limit/friends.jpg',
+    },
+  },
+  {
+    id: 2,
+    date: '03/05/23',
+    time: '19:00 GMT',
+    location: "Monica's House",
+    chosen_book: {
+      authors: ['Charlotte Brontë'],
+      title: 'Jane Eyre',
+      cover_photo:
+        'https://m.media-amazon.com/images/I/A1Xy2f66SmL._SL1500_.jpg',
+    },
+    bookclub: {
+      name: 'The Creative Chronicles',
+      image:
+        'https://assets.goodereader.com/blog/uploads/images/2022/03/27085515/104702698-GettyImages-583816330-book-club.jpg',
+    },
+  },
+  {
+    id: 3,
+    date: '12/08/23',
+    time: '21:00 GMT',
+    location: 'Online - Zoom',
+    chosen_book: {
+      authors: ['Lee Child', 'Andrew Child'],
+      title: 'The Secret',
+      cover_photo:
+        'https://storage.googleapis.com/circlesoft/document/photos/004/060/950/original_the_secret.jpg?1699933046',
+    },
+    bookclub: {
+      name: 'Chapter Chasers',
+      image:
+        'https://images.squarespace-cdn.com/content/v1/613b5cf167750b42b480da2b/17d0174e-1acd-4b31-b917-cd1ed69063a5/bookclub+article+2.jpg',
+    },
+  },
+  {
+    id: 4,
+    date: '03/06/23',
+    time: '19:00 GMT',
+    location: "Jane's House",
+    chosen_book: {
+      authors: ['Judy Nunn'],
+      title: 'Black Sheep',
+      cover_photo:
+        'https://storage.googleapis.com/circlesoft/document/photos/003/997/234/large_temp_img.jpg20231113-1-172w3f7?1699837376',
+    },
+    bookclub: {
+      name: 'The Creative Chronicles',
+      image:
+        'https://assets.goodereader.com/blog/uploads/images/2022/03/27085515/104702698-GettyImages-583816330-book-club.jpg',
+    },
+  },
+];
