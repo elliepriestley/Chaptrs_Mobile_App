337a0d7dc18657c39f9d51bef495f712c15ae6c5
1sAndZeros <rikie_p@hotmail.com>
2023-11-23 20:12:52 +0000

    Fix header style and add authentication functionality

--- frontend/screens/sessionDetails.js
+++ frontend/screens/sessionDetails.js
@@ -9,7 +9,6 @@ import {
 import Heading from '../components/Heading';
 import { Location, Calendar, Clock } from 'iconsax-react-native';
 import AvatarGroup from '../components/AvatarGroup';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 function SessionDetailsScreen({ route }) {
   const session = route.params?.session;
