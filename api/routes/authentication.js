dcda1953e67344bbc33199a557ba517ef1f0e227
alalina <alalinaerm@gmail.com>
2023-12-19 18:17:52 +0000

    added bookclun details screeen

--- api/routes/authentication.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const AuthenticationController = require("../controllers/authentication");
-
-router.post("/", AuthenticationController.Authenticate);
-
-module.exports = router;
--- frontend/data/schemas/loginSchema.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from 'yup';
-
-const loginSchema = Yup.object({
-  email: Yup.string().email('Email is not valid').required('Email is required'),
-  password: Yup.string().required('Password is required'),
-});
-
-export default loginSchema;
