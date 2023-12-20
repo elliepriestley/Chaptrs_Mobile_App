ea15e6c062ef18780661b0af93f2744c27e3b220
1sAndZeros <rikie_p@hotmail.com>
2023-12-20 16:32:23 +0000

    update dependencies and add password encryption

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,25 +1,30 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const TokenGenerator = require('../lib/token_generator');
 
 const AuthenticationController = {
-  Authenticate: (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+  Authenticate: async (req, res) => {
+    try {
+      const email = req.body.email;
+      const password = req.body.password;
 
-    User.findOne({ email: email }).then((user) => {
+      const user = await User.findOne({ email: email });
       if (!user) {
         console.log('auth error: user not found');
-        res.status(401).json({ message: 'Login details incorrect' });
-      } else if (user.password !== password) {
+        res.status(401).json({ message: 'Login details incorrectwww' });
+      }
+      const passwordsMatch = await bcrypt.compare(password, user.password);
+      if (!passwordsMatch) {
         console.log('auth error: passwords do not match');
         res.status(401).json({ message: 'Login details incorrect' });
-      } else {
-        const token = TokenGenerator.jsonwebtoken(user.id);
-        res
-          .status(201)
-          .json({ token: token, message: 'User authorised', user: user });
       }
-    });
+      const token = TokenGenerator.jsonwebtoken(user.id);
+      res
+        .status(201)
+        .json({ token: token, message: 'User authorised', user: user });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   },
 };
 
