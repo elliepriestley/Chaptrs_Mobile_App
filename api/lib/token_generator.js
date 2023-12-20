62510f861f61cd6c8d4306b255e3e63f17e650ed
1sAndZeros <rikie_p@hotmail.com>
2023-12-20 15:50:22 +0000

    set token expiration to 1 day using JWT options, auto set iat

--- api/lib/token_generator.js
+++ api/lib/token_generator.js
@@ -1,4 +1,4 @@
-const JWT = require("jsonwebtoken");
+const JWT = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 
 /**
@@ -6,15 +6,21 @@ const secret = process.env.JWT_SECRET;
  * token for a specific user.
  */
 class TokenGenerator {
-    static jsonwebtoken(user_id) {
-        return JWT.sign({
-            user_id: user_id,
-            iat: Math.floor(Date.now() / 1000),
+  static jsonwebtoken(user_id) {
+    return JWT.sign(
+      {
+        user_id: user_id,
+        iat: Math.floor(Date.now() / 1000),
 
-            // Set the JWT token to expire in 20 minutes
-            exp: Math.floor(Date.now() / 1000) + (20 * 60)
-        }, secret);
-    }
+        // Set the JWT token to expire in 1 day ( 24 hours * 60 minutes * 60 seconds)
+        // exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
+      },
+      secret,
+      {
+        expiresIn: '1d', // sets exp key in token
+      },
+    );
+  }
 }
 
 module.exports = TokenGenerator;
