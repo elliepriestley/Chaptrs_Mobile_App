514d1ebb7b8ae963b6135c1c6e79e8a777abf2a1
1sAndZeros <rikie_p@hotmail.com>
2023-12-20 15:50:38 +0000

    set token expiration to 1 day using JWT options, auto set iat

--- api/lib/token_generator.js
+++ api/lib/token_generator.js
@@ -10,10 +10,6 @@ class TokenGenerator {
     return JWT.sign(
       {
         user_id: user_id,
-        iat: Math.floor(Date.now() / 1000),
-
-        // Set the JWT token to expire in 1 day ( 24 hours * 60 minutes * 60 seconds)
-        // exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
       },
       secret,
       {
